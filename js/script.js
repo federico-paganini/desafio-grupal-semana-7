b53b236f452f315a5e5af53da764b26b903aba71
federico-paganini <perfedefer@gmail.com>
2023-08-08 20:38:30 -0300

    fetch agregado

--- js/script.js
+++ js/script.js
@@ -19,3 +19,16 @@ function showData(dataArray) {
 }
 
 // Escribe el código necesario para realizar el fetch al archivo con los datos y mostrar los estudiantes con la función showData
+
+fetch(DATA_URL).then(response =>
+  {
+    if(response.ok)
+    {
+      return response.json();
+    }
+    else {
+      console.log("Hay Error");
+    }
+  }).then(respuestaJson =>{
+    showData(respuestaJson.students);
+  });
